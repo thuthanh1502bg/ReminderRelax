f015b7620acdcc073017c6c3ec4b6608bc6e299b
Thu thành <thuthanhlmlnbg1502@gmail.com>
2021-01-28 23:23:38 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿using System;
+
+namespace ReminderRelax
+{
+    partial class Form1
+    {
+        // Khai bao bien bat buoc
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.mnuNotify = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showDialogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.udInterval = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtSound = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSound = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.mnuNotify.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.udInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipTitle = "Lời nhắc:";
+            this.notifyIcon1.ContextMenuStrip = this.mnuNotify;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Nhắc nhở nghỉ ngơi";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipShown += new System.EventHandler(this.notifyIcon1_BalloonTipShown);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // mnuNotify
+            // 
+            this.mnuNotify.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuNotify.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showDialogToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.mnuNotify.Name = "mnuNotify";
+            this.mnuNotify.Size = new System.Drawing.Size(178, 58);
+            // 
+            // showDialogToolStripMenuItem
+            // 
+            this.showDialogToolStripMenuItem.Name = "showDialogToolStripMenuItem";
+            this.showDialogToolStripMenuItem.Size = new System.Drawing.Size(177, 24);
+            this.showDialogToolStripMenuItem.Text = "Mở hộp thoại...";
+            this.showDialogToolStripMenuItem.Click += new System.EventHandler(this.showDialogToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(174, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(177, 24);
+            this.exitToolStripMenuItem.Text = "Thoát";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dtStart
+            // 
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtStart.Location = new System.Drawing.Point(150, 18);
+            this.dtStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(201, 27);
+            this.dtStart.TabIndex = 0;
+            this.dtStart.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Thời gian bắt đầu:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label3.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label3.Location = new System.Drawing.Point(13, 74);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nhắc lại sau:";
+            // 
+            // udInterval
+            // 
+            this.udInterval.CausesValidation = false;
+            this.udInterval.Location = new System.Drawing.Point(150, 72);
+            this.udInterval.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.udInterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.udInterval.Name = "udInterval";
+            this.udInterval.ReadOnly = true;
+            this.udInterval.Size = new System.Drawing.Size(84, 27);
+            this.udInterval.TabIndex = 6;
+            this.udInterval.UseWaitCursor = true;
+            this.udInterval.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label4.Location = new System.Drawing.Point(253, 74);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "phút.";
+            this.label4.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label5.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label5.Location = new System.Drawing.Point(13, 128);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Lời nhắc nhở:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtMessage.Location = new System.Drawing.Point(150, 121);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(201, 27);
+            this.txtMessage.TabIndex = 9;
+            // 
+            // txtSound
+            // 
+            this.txtSound.Location = new System.Drawing.Point(150, 177);
+            this.txtSound.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSound.Name = "txtSound";
+            this.txtSound.Size = new System.Drawing.Size(201, 27);
+            this.txtSound.TabIndex = 9;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.Location = new System.Drawing.Point(38, 266);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(117, 37);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Tạo nhắc nhở";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label6.Location = new System.Drawing.Point(13, 180);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Âm thanh:";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Location = new System.Drawing.Point(303, 266);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(117, 37);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Xóa lời nhắc";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSound
+            // 
+            this.btnSound.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnSound.Location = new System.Drawing.Point(359, 177);
+            this.btnSound.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSound.Name = "btnSound";
+            this.btnSound.Size = new System.Drawing.Size(33, 27);
+            this.btnSound.TabIndex = 10;
+            this.btnSound.Text = "...";
+            this.btnSound.UseVisualStyleBackColor = true;
+            this.btnSound.Click += new System.EventHandler(this.btnSound_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlay.Location = new System.Drawing.Point(298, 212);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(94, 29);
+            this.btnPlay.TabIndex = 10;
+            this.btnPlay.Text = "Nghe thử";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStop.Location = new System.Drawing.Point(163, 266);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(132, 37);
+            this.btnStop.TabIndex = 10;
+            this.btnStop.Text = "Ngưng nhắc nhở";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(447, 340);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtSound);
+            this.Controls.Add(this.btnSound);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.udInterval);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtStart);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nhắc nhở nghỉ ngơi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mnuNotify.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.udInterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+      
+
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown udInterval;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ContextMenuStrip mnuNotify;
+        private System.Windows.Forms.ToolStripMenuItem showDialogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSound;
+        private System.Windows.Forms.TextBox txtSound;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ReminderRelax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDialogFields();
+            InitializeSound();
+        }
+
+        // Khoi tao hop thoai
+        private void InitializeDialogFields()
+        {
+            dtStart.Value = DateTime.Now;
+            btnStop.Enabled = false;
+        }
+
+
+
+        // Loi nhac nho
+        private void InitializeNotifyBalloon()
+        {
+            notifyIcon1.BalloonTipTitle = "Lời nhắc yêu thương";
+            notifyIcon1.BalloonTipText = txtMessage.Text;
+        }
+
+        private void SetupTimer()
+        {
+            // Chuyen doi tu phut sang giay 
+            timer1.Interval = (int)udInterval.Value * 1000 * 60; 
+            timer1.Start();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Hien thi thong bao tao nhac nho thanh cong
+            MessageSuccess();
+
+            // Tao icon nho goc man hinh va setup thoi gian
+            InitializeNotifyBalloon();
+            SetupTimer();
+
+            // Khoi tao tinh Enable cua cac phan tu khac
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            btnReset.Enabled = false;
+            dtStart.Enabled = false;
+            udInterval.Enabled = false;
+            txtMessage.Enabled = false;
+            txtSound.Enabled = false;
+
+            // An ung dung
+            this.ShowInTaskbar = false;
+            this.Hide();
+        }
+
+
+        // Khoi tao am thanh
+        private void InitializeSound()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = txtSound.Text;
+        }
+
+
+
+        // Khi nhan vao nut am thanh
+        private void btnSound_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog() { Filter = "Audio File (*.mp3;*.wav)|*.mp3;*.wav;", Multiselect = false, ValidateNames = true };
+            {
+                if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    this.txtSound.Text = file.FileName;
+                }
+
+            }
+        }
+
+
+        // Hien thi thong bao tao nhac nho thanh cong
+        private void MessageSuccess()
+        {
+            string message = "Bạn đã tạo nhắc nhở thành công. Chúc bạn một ngày tốt lành! ";
+            string title = "Thông báo";
+            MessageBox.Show(message, title);
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Kiem tra thoi gian
+            if (CheckIfStartTimeHappened())
+            {
+                // Hien thi thong bao
+                ShowReminder();
+            }
+        }
+
+
+        // Kiem tra thoi gian 
+        private bool CheckIfStartTimeHappened()
+        {
+            if (DateTime.Now > dtStart.Value)
+                return true;
+
+            return false;
+        }
+
+
+
+        // Hien thi thong bao
+        private void ShowReminder()
+        {
+            notifyIcon1.ShowBalloonTip(2);
+            PlaySound();
+        }
+
+
+        // Nut nghe thu am thanh thong bao
+        private void PlaySound()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = txtSound.Text;
+            player.Play();
+        }
+
+
+        // Cai dat hop thoai khong bi chinh sua khi ung dung chưa bat
+        private void notifyIcon1_BalloonTipShown(object sender, EventArgs e)
+        {
+            if (this.Visible == false)
+            {
+                System.Threading.Thread.Sleep(8000);
+                notifyIcon1.Visible = false;
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        // Hien thi hop thoai khi click chuot hai lan
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            notifyIcon1.Visible = true;
+            this.Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        // Ham xoa thong tin trong o loi nhac nho va am thanh
+        void Clear()
+        {
+            txtMessage.Clear();
+            txtSound.Clear();
+            InitializeDialogFields();
+
+        }
+
+        // Click nut xoa loi nhac nho
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        // Click nut nghe thu
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            PlaySound();
+        }
+
+        // Click nut ngung nhac nho
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Dong ho dem thoi gian ngung
+            timer1.Stop();
+
+            // Khoi tao lai tinh Enable cua cac phan tu khac
+            btnStart.Enabled = true;
+            btnReset.Enabled = true;
+            btnStop.Enabled = false;
+            dtStart.Enabled = false;
+            udInterval.Enabled = true;
+            txtMessage.Enabled = true;
+            txtSound.Enabled = true;
+        }
+
+
+        // Hien thi ung dung
+        private void showDialogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+
+        // Thoat ung dung
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
